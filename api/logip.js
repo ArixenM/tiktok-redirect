34ab3fa72a3ff16452ab6d7c3a5a4ebc9fd1564b
Arixen <73908677+ArixenM@users.noreply.github.com>
2025-10-05 22:25:51 -0400

    Update logip.js

--- api/logip.js
+++ api/logip.js
@@ -2,9 +2,12 @@
 export default function handler(req, res) {
   if (req.method === 'POST') {
     const ip = req.body.ip || req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-    console.log('Logged IP address:', ip); // visible in Vercel logs
+    console.log('Logged IP address:', ip); // shows in Vercel logs
     res.status(200).send('OK');
+  } else if (req.method === 'GET') {
+    res.status(200).send('Server is running');
   } else {
     res.status(405).send('Method Not Allowed');
   }
 }
+
